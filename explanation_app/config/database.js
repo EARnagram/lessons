7fcc72b07b99852f7beaf005d81d8e89eb6c3c7c
Ezra Raez <ezRAez10@gmail.com>
2016-01-21 15:58:33 -0800

    reorganize lessons

--- explanation_app/config/database.js
+++
@@ -1,8 +0,0 @@
-// Require mongoose to speak to your databse.
-var mongoose = require('mongoose');
-
-// connect the project to your database
-mongoose.connect('mongodb://localhost/explanation_app');
-
-// export the connection
-module.exports = mongoose;
