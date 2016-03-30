00632872a440205f20431d5cf3ac0d5c8adf20d6
Ezra Raez <ezRAez10@gmail.com>
2016-03-30 12:00:04 -0700

    update deploy mean lesson

--- sec4/build_deploy_mean_app/config_walkthrough_starter/derrys_fishin_friends/app/config/database.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-// connect to db
-mongoose.connect('mongodb://localhost/derry_fishin_log');
-
-// export the connection
-module.exports = mongoose;
--- sec4/build_deploy_mean_app/starter/derrys_fishin_friends/app/config/environment.js
+++
@@ -1,9 +0,0 @@
-var _ = require('lodash');
-
-var localEnvVars = {
-  TITLE:      'derry_fishin_log',
-  SAFE_TITLE: 'derry_fishin_log'
-};
-
-// Merge all environmental variables into one object.
-module.exports = _.extend(process.env, localEnvVars);
