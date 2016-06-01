b94a528e938e1fc5f07f69ffb34a49081fb3e2b9
Ezra Raez <ezRAez10@gmail.com>
2016-05-31 20:13:17 -0700

    apply get routes for logs, experiments, and scientists

--- sec2/nested_routes_forms/solution_code/app/controllers/logs_controller.rb
+++ sec2/nested_routes_forms/solution_code/app/controllers/logs_controller.rb
@@ -2,5 +2,8 @@ class LogsController < ApplicationController
 
   def show
     @log = Log.find(params[:id])
+    @logs = Log.where(subject_name: @log.subject_name)
+    @experiment = @log.experiment
+    @scientist = @log.experiment.scientist
   end
 end
