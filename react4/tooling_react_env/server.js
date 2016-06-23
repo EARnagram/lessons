3d634b0814a7db2913b2f4814b302ee27c898fd1
Ezra Raez <ezRAez10@gmail.com>
2016-06-23 15:33:07 -0700

    create template

---
+++ react4/tooling_react_env/server.js
@@ -0,0 +1,20 @@
+// only need server.js if using hot reload for a front end application.
+// if you have a backend, you should not need webpack dev server because
+// you would serve your site from your backend server!
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(8080, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Listening at http://localhost:8080/');
+});
+
---
+++ react4/tooling_react_env/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Wushup errybody?!</h1>
+    );
+  }
+}
+
+export default App;
